8e4ce31e03a60b4855229c1d7552e7dca9953a6c
Raudel Flores <rawdell@outlook.com>
2019-10-08 17:11:18 -0500

    finished the first principle

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,19 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
+* 1. When the function is contained in the global scope,
+    the value of the inside function will be the window object.
 * 2. 
 * 3. 
 * 4. 
 *
 * write out a code example of each explanation above
-*/
+*/scopes = scope => {
+    console.log(this);
+    return `When the function is contained in the ${scope} scope,
+    the value of the inside function will be the window object.`;
+}
+console.log(scopes('Global'));
 
 // Principle 1
 
